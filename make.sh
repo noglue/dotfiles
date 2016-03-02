6fa0f0fcd6792137d18a9ca124880c9a1303119b
basti <git@bastimerk.de>
2016-03-02 15:14:13 +0100

    olddir in den dotfiles-Ordner umgezogen

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+olddir=$dir/dotfiles_old             # old dotfiles backup directory
 files="bashrc  dircolors vim  vimrc"
 ##########
 
